a544d301bc557a60ab54119e9357c611d8e244bb
Ravishankar paribrajak <paribrajakravishankarkumar@gmail.com>
2026-05-24 22:12:34 +0530

    Added Screen with background image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
-import { Image } from "expo-image";
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+
 
 const imagePlaceholder = require("@/assets/images/background.jpg");
 
@@ -8,7 +9,11 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={imagePlaceholder} style={styles.image} />
+        <ImageViewer imgSource={imagePlaceholder} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -17,24 +22,15 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: '#fff'
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: 'yellow'
+    backgroundColor: '#25292e',
+    alignItems: 'center',
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
